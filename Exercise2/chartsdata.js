8c5545962e9d11284ddcf41641059b40dd5d183c
Alicia Daza <amdazacastillo@gmail.com>
2021-05-03 23:03:08 +0200

    use properties file

--- Exercise2/chartsdata.js
+++ Exercise2/chartsdata.js
@@ -1,9 +1,16 @@
-fetch('http://s3.amazonaws.com/logtrust-static/test/test/data1.json').then(function(response) {
-  return response.json();
-}).then(function(json) {
-  products = json;
-  console.log(products);
-  initialize();
-}).catch(function(err) {
-  console.log('Fetch problem: ' + err.message);
-});
+// Get URL's with data
+
+const props = window.ycs_properties;
+console.log(props);
+
+// fetch('http://s3.amazonaws.com/logtrust-static/test/test/data1.json').then(function(response) {
+//   return response.json();
+// }).then(function(json) {
+//   const products = json;
+//   console.log("PRODUCTS: " + products);
+//   initialize();
+// }).catch(function(err) {
+//   console.log('Fetch problem: ' + err.message);
+// });
+
+// TODO: get 3 promises and join
---
+++ Exercise2/properties.js
@@ -0,0 +1,7 @@
+ycs_properties = {
+    "data_urls": [
+        "http://s3.amazonaws.com/logtrust-static/test/test/data1.json",
+        "http://s3.amazonaws.com/logtrust-static/test/test/data2.json",
+        "http://s3.amazonaws.com/logtrust-static/test/test/data3.json"
+    ]
+}
